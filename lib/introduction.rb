f70c0fc60cd152e759e2b1a97006db2166c2e48d
Daniel Calderon <danieleduardocald@gmail.com>
2020-11-18 15:34:43 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,6 +11,13 @@ end
 
 introduction_with_language("Peter","C++")
 
+def introduction_with_language_optional(name, language= "ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language("Peter","C++")
+
+
 
 
 
